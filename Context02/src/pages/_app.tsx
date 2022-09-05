06bbd0a0b8a7ad46d58eedcf3866edbef9ca977f
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-09-06 01:18:32 +0900

    ♻️ todosに置き換え＆型付け

---
+++ Context02/src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import type { AppProps } from "next/app";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { Layout } from "src/components/Layout";
+import { Todo } from "src/types";
+
+const TODOS: Todo[] = [
+  { id: 1, text: "foo", isDone: false },
+  { id: 2, text: "bar", isDone: true },
+];
+
+export type TodoContext ={
+  todos: Todo[];
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
+}
+
+export const TodoContext = createContext<TodoContext>({
+  todos: TODOS,
+  setTodos: () => {
+    throw Error("No default value!");
+  },
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+
+  return (
+    <TodoContext.Provider value={{ todos, setTodos }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </TodoContext.Provider>
+  );
+};
+
+export default MyApp;
