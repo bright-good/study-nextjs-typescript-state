e768f73187510b3794b25fbf48437c3d43828442
akira-wd-1224 <jda.7seven.a24@gmail.com>
2022-09-07 10:32:47 +0900

    ♻️  Reduxに置き換え

---
+++ Redux01/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { Layout } from "src/components/Layout";
+import { store } from "src/state";
+import { Todo } from "src/types";
+
+const TODOS: Todo[] = [
+  { id: 1, text: "foo", isDone: false },
+  { id: 2, text: "bar", isDone: true },
+];
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
+}
